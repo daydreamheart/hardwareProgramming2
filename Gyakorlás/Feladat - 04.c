be0d75b6fd8ab13a5d38d07f8154807f745bd3f7
Melinda <m4gy4r.m3l1nd4@gmail.com>
2020-05-22 15:50:37 +0200

    Add files via upload

---
+++ Gyakorlás/Feladat - 04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc == 1)
+        printf("Hello, %s!\n", getenv("LOGNAME"));
+    else if ((argc == 2) && (strcmp(argv[1], "-hu") == 0)) {
+        printf("Hello, %s!\n", getenv("LOGNAME"));
+        return 0;
+    }
+    else if ((argc == 3) && (strcmp(argv[1], "-hu") == 0)) {
+        printf("Hello, %s!\n", argv[2]);
+        return 0;
+    }
+    else
+        printf("Hello, %s!\n", argv[1]);
+
+    return 0;
+}
