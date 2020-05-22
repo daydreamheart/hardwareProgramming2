38a5fb1cabad77f177a722ff9a90b296a5e81b26
Melinda <m4gy4r.m3l1nd4@gmail.com>
2020-05-22 15:51:54 +0200

    Add files via upload

---
+++ Órai - 01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    if(argc == 1)
+        return 1;
+    
+    for(int i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
+
+    return 0;
+
+    // gcc 01.c -o 01
+    // ./01 alma 1 2 3 4
+    // ./01 "alma 1"
+}
